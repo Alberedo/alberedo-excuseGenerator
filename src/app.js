3d754227044eb1f2655d59c8a5a2dfc4ea40e83a
Alberto Madrid <albertomadrid93@gmail.com>
2025-09-03 18:50:53 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
+
+let who = ["Mi perro ", "Mi padre ", "Un señor que pasaba por la calle ", "La madre de Javier, el primo de Lourdes ", "El raton de la cocina ", "El ratoncito Perez "]
+let action= ["se ha comido ", "ha perdido ", "me ha pedido urgentemente ", "ha explotado ", "ha cambiado ", "intentó hacer sin exito "]
+let what= ["mis deberes ", "mi coche ", "el avión familiar ", "a la abuela ", "una ballena beluga "]
+let when= ["al salir de clase.", "esta mañana al amanecer.", "cuando he llegado a casa.", "despues de los examenes.", "en una dimensión paralela."]
+let excuses = [who, action, what, when]
+
+
+function generateExcuse(){{
+  let excuse=""
+  for(let element of excuses){
+    excuse += element[Math.floor(Math.random()*element.length)]    
+  }
+  return excuse
+}}
+
+document.getElementById("excuse").innerHTML = generateExcuse()
+
 };
